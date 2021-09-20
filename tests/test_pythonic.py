cbff68ffe7198fc32c307ecaec3654e214410050
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-09-20 17:05:28 +0300

    Update test_pythonic.py

--- tests/test_pythonic.py
+++ tests/test_pythonic.py
@@ -5,7 +5,12 @@ from pythonic.pythonic import *
 def test_version():
     assert __version__ == '0.1.0'
 
-def test_factors_one_and_factors_two():
-    actual = factors_two(100)
+def test_factors_one():
+    actual = factors_one(100)
     expected = [1, 2, 4, 5, 10, 20, 25, 50, 100]
     assert actual == expected
+
+def test_factors_two():
+    actual = [1, 2, 4, 5, 10, 20, 25, 50, 100]
+    expected = for idx, factor in factors_two(100):
+    assert item == actual[idx]
