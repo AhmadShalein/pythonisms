6c1725d0308b0632d80df67d21924495be2eec69
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-09-20 17:09:31 +0300

    Update test_pythonic.py

--- tests/test_pythonic.py
+++ tests/test_pythonic.py
@@ -9,8 +9,3 @@ def test_factors_one():
     actual = factors_one(100)
     expected = [1, 2, 4, 5, 10, 20, 25, 50, 100]
     assert actual == expected
-
-def test_factors_two():
-    actual = [1, 2, 4, 5, 10, 20, 25, 50, 100]
-    expected = for idx, factor in factors_two(100):
-    assert item == actual[idx]
